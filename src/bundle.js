20589f2d18fb0141ad454527d48b85d666cdffc2
Santiago Moran <santimoran.1@hotmail.com>
2019-12-28 21:42:25 -0300

    ICE server test

--- src/bundle.js
+++ src/bundle.js
@@ -5287,29 +5287,63 @@ function config (name) {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],27:[function(require,module,exports){
+// navigator.webkitGetUserMedia(
+// { video: true, audio: false },
+// stream => {
 const Peer = require('simple-peer')
 const peer = new Peer({
-    initiator: location.hash === '#init',
-    trickle: false
+	initiator: location.hash === '#init',
+	trickle: false,
+	// stream: stream
+	config: {
+		iceServers: [
+			{
+				urls: [
+					'stun:stun.l.google.com:19302',
+					'stun:stun1.l.google.com:19302',
+					'stun:stun2.l.google.com:19302',
+					'stun:stun.l.google.com:19302?transport=udp'
+				]
+			},
+			{
+				urls: 'turn:webrtcweb.com:7788',
+				username: 'muazkh',
+				credential: 'muazkh'
+			}
+		]
+	}
 })
 
 peer.on('signal', data => {
-    document.getElementById('yourId').value = JSON.stringify(data)
+	document.getElementById('yourId').value = JSON.stringify(data)
 })
 
 document.getElementById('connect').addEventListener('click', () => {
-    const otherId = JSON.parse(document.getElementById('otherId').value)
-    peer.signal(otherId)
+	const otherId = JSON.parse(document.getElementById('otherId').value)
+	peer.signal(otherId)
 })
 
 document.getElementById('send').addEventListener('click', () => {
-    const yourMessage = document.getElementById('yourMessage').value
-    peer.send(yourMessage)
+	const yourMessage = document.getElementById('yourMessage').value
+	peer.send(yourMessage)
 })
 
 peer.on('data', data => {
-    document.getElementById('messages').textContent += data + '\n'
+	document.getElementById('messages').textContent += data + '\n'
 })
+
+peer.on('stream', stream => {
+	const video = document.createElement('video')
+	document.body.appendChild(video)
+	video.src = window.URL.createObjectURL(stream)
+	video.play()
+})
+// 	},
+// 	err => {
+// 		console.log(err)
+// 	}
+// )
+
 },{"simple-peer":6}],28:[function(require,module,exports){
 'use strict'
 
