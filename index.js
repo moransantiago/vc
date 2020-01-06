d603da5ad80ceb6e6d2c77d3fb226e8a906b42f0
Santiago Moran <santimoran.1@hotmail.com>
2020-01-06 03:47:08 -0300

    A few details corrected

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const path = require('path')
 
-const { bindSignalingEvents } = require('./sockets/signaling') // => Function to
+const { bindSignalingEvents } = require('./sockets/signaling') // => Function to set the events on the future sockets
 
 app.use('/', express.static(path.join(__dirname, 'src')))
 
--- src/bundle.js
+++ src/bundle.js
@@ -11538,11 +11538,43 @@ module.exports = yeast;
 const Peer = require('simple-peer')
 const io = require('socket.io-client')
 
-const gotUserMedia = stream => {
-	// => This will be executed if the stream is successfully obtained
-	const localVideo = document.getElementById('localVideo')
-	localVideo.srcObject = stream
-	const peer = new Peer({
+const gotUserMedia = stream => { // => This will be executed if the stream is successfully obtained
+	bindVideoToHtml(document.getElementById('localVideo'), stream) // => Bind the media that we recieve from the user into localVideo html element
+	const peer = createPeer(stream)
+	const socket = io('http://localhost:3000/ws/signaling')
+
+	socket.on('connect', () => {
+		socket.on('signal', offerOrAnswer => {
+			peer.signal(offerOrAnswer)
+		})
+	})
+
+	peer.on('signal', data => {
+		const offerOrAnswer = JSON.stringify(data)
+		socket.emit('signal', offerOrAnswer) // => Whenever a peer recieves a signal, it emit the signal event to every connected peer
+	})
+
+	peer.on('connect', () => {
+		console.log('A peer has just connected')
+	})
+
+	peer.on('data', data => {
+		document.getElementById('messages').textContent += data + '\n'
+	})
+
+	peer.on('stream', stream => {
+		bindVideoToHtml(document.getElementById('remoteVideo'), stream) // => Bind the stream that we recieve from the other peer into remoteVideo html element
+		alert('Got track')
+	})
+
+	document.getElementById('send').addEventListener('click', () => {
+		const yourMessage = document.getElementById('yourMessage').value
+		peer.send(yourMessage)
+	})
+}
+
+const createPeer = stream => (
+	new Peer({
 		initiator: location.hash === '#init',
 		trickle: false,
 		stream: stream,
@@ -11564,52 +11596,19 @@ const gotUserMedia = stream => {
 			]
 		}
 	})
+)
 
-	const socket = io('http://localhost:3000/ws/signaling')
-
-	socket.on('connect', () => {
-		socket.on('signal', offerOrAnswer => {
-			peer.signal(offerOrAnswer)
-		})
-	})
-
-	peer.on('signal', data => {
-		console.log(data)
-		socket.emit('signal', data) // => Whenever a peer recieves a signal, it emit the signal event to every connected peer
-	})
-
-	document.getElementById('connect').addEventListener('click', () => {
-		const otherId = JSON.parse(document.getElementById('otherId').value)
-		peer.signal(otherId)
-	})
-
-	document.getElementById('send').addEventListener('click', () => {
-		const yourMessage = document.getElementById('yourMessage').value
-		peer.send(yourMessage)
-	})
-
-	peer.on('data', data => {
-		document.getElementById('messages').textContent += data + '\n'
-	})
-
-	peer.on('track', stream => {
-		// const remoteVideo = document.getElementById('remoteVideo')
-		// if ('srcObject' in remoteVideo) {
-		// 	remoteVideo.srcObject = stream
-		// } else {
-		// 	remoteVideo.src = window.URL.createObjectURL(stream) // for older browsers
-		// }
-		// remoteVideo.play()
-		alert('Got track')
-	})
+const bindVideoToHtml = (DOMElement, stream) => {
+	if ('srcObject' in DOMElement) {
+		DOMElement.srcObject = stream
+	} else {
+		DOMElement.src = window.URL.createObjectURL(stream) // => For older browsers
+	}
 }
 
 const sale = async () => {
 	try {
-		const stream = await navigator.mediaDevices.getUserMedia({
-			video: false,
-			audio: true
-		})
+		const stream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true})
 		gotUserMedia(stream)
 	} catch (error) {
 		console.log(`Error: ${error.message}`)
