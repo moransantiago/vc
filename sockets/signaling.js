94c8814b2cd6c87e94f31038f9712404021b8d09
Santiago Moran <santimoran.1@hotmail.com>
2020-01-22 18:16:14 -0300

    Signaling server can now perform multiple peer signaling

--- sockets/signaling.js
+++ sockets/signaling.js
@@ -13,12 +13,14 @@ module.exports = {
 				socket.broadcast.to(room).emit('signal', offerOrAnswer)
 			})
 
-			socket.on('join', room => {
+			socket.on('join', ({ id, room }) => {
 				// => When the server recieves a join, it checks if there are empty and then joins the socket room
 				socket.join(room, () => {
-					const amountOfSocketsInRoom = Object.keys(io.adapter.rooms[room].sockets).length // => This is the amount of sockets joined to this room
+					const amountOfSocketsInRoom = Object.keys(
+						io.adapter.rooms[room].sockets
+					).length // => This is the amount of sockets joined to this room
 					if (amountOfSocketsInRoom < 2) socket.emit('initiator') // => If this is the only socket in this room, we notify the client this so he wont perform signaling
-					socket.broadcast.to(room).emit('joiner') // => Broadcast to everyone in the room that a new user has joined
+					socket.broadcast.to(room).emit('joiner', id) // => Broadcast to everyone in the room that a new user has joined
 				})
 			})
 		})
--- src/main.js
+++ src/main.js
@@ -1,38 +1,76 @@
 const Peer = require('simple-peer')
 const io = require('socket.io-client')
+const myId = prompt('Insert your username')
 let myOffer
-let isInARoom = false
 let imInitiator = false
+const peers = [] // => Will be an array of objects: { id: <name>, peer: new Peer() }
 
 const gotUserMedia = stream => {
 	// => This will be executed if the stream is successfully obtained
 	bindVideoToHtml(document.getElementById('localVideo'), stream) // => Bind the media that we recieve from the user into localVideo html element
-	const peer = createPeer(stream)
 	const socket = io('http://localhost:3000/ws/signaling')
 
 	socket.on('connect', () => {
-		socket.on('signal', offerOrAnswer => {
-			peer.signal(offerOrAnswer)
+		socket.on('signal', ({ id, offerOrAnswer, peerId }) => {
+			if (offerOrAnswer.type === 'offer' && id === myId) {
+				const newPeer = createPeer(false)
+				setPeerEvents(newPeer, socket, peerId)
+				newPeer.signal(offerOrAnswer)
+				peers.push({ id: peerId, peer: newPeer })
+			} else if (offerOrAnswer.type === 'answer' && id === myId) {
+				const signaledFrom = peers.filter(peer => peer.id === peerId)
+				signaledFrom[0].peer.signal(offerOrAnswer)
+			}
 		})
 
-		socket.on('joiner', () => {
+		socket.on('joiner', id => {
 			console.log('A new user has joined this room')
+			const newPeer = createPeer(true)
+			setPeerEvents(newPeer, socket, id)
+			peers.push({ id, peer: newPeer })
 		})
 
-		socket.on('initiator', () => { imInitiator = true })
+		socket.on('initiator', () => {
+			imInitiator = true
+		})
 	})
 
-	peer.on('signal', data => {
-		const offerOrAnswer = JSON.stringify(data)
-		if (isInARoom) {
-			socket.emit('signal', offerOrAnswer) // => Whenever a peer recieves a signal, it emit the signal event to every connected peer
-		} else {
-			myOffer = offerOrAnswer
+	document.getElementById('room1').addEventListener('click', () => {
+		socket.emit('join', { id: myId, room: 'room1' }) // => PEERS ALWAYS HAVE TO EMIT THIS EVENT TO CONNECT TO THE ROOM BEFORE THEY EMIT SIGNAL EVENT
+		if (myOffer && !imInitiator) {
+			socket.emit('signal', { id: peer.otherId, offerOrAnswer: myOffer, peerId: myId })
 		}
+	})
+
+	document.getElementById('room2').addEventListener('click', () => {
+		socket.emit('join', { id: myId, room: 'room2' }) // => PEERS ALWAYS HAVE TO EMIT THIS EVENT TO CONNECT TO THE ROOM BEFORE THEY EMIT SIGNAL EVENT
+		if (myOffer && !imInitiator) {
+			socket.emit('signal', { id: peer.otherId, offerOrAnswer: myOffer, peerId: myId })
+		}
+	})
+
+	document.getElementById('room3').addEventListener('click', () => {
+		socket.emit('join', { id: myId, room: 'room3' }) // => PEERS ALWAYS HAVE TO EMIT THIS EVENT TO CONNECT TO THE ROOM BEFORE THEY EMIT SIGNAL EVENT
+		if (myOffer && !imInitiator) {
+			socket.emit('signal', { id: peer.otherId, offerOrAnswer: myOffer, peerId: myId })
+		}
+	})
+}
+
+const setPeerEvents = (peer, socket, id) => {
+	peer.otherId = id // => To perform signaling
+
+	peer.on('signal', offerOrAnswer => {
+		socket.emit('signal', { id: peer.otherId, offerOrAnswer, peerId: myId })
+		// => Whenever a peer recieves a signal, it emit the signal event to every connected peer
 	})
 
 	peer.on('connect', () => {
 		console.log('A peer has just connected')
+	})
+
+	peer.on('error', err => {
+		console.log(err)
 	})
 
 	peer.on('data', data => {
@@ -40,54 +78,20 @@ const gotUserMedia = stream => {
 	})
 
 	peer.on('stream', stream => {
-		bindVideoToHtml(document.getElementById('remoteVideo'), stream) // => Bind the stream that we recieve from the other peer into remoteVideo html element
-		alert('Got track')
-	})
-
-	document.getElementById('send').addEventListener('click', () => {
-		const yourMessage = document.getElementById('yourMessage').value
-		peer.send(yourMessage)
-	})
-
-	document.getElementById('room1').addEventListener('click', () => {
-		if (myOffer && !imInitiator) {
-			socket.emit('signal', myOffer)
-		}
-		socket.emit('join', 'room1')
-		isInARoom = true
-	})
-
-	document.getElementById('room2').addEventListener('click', () => {
-		socket.emit('join', 'room2')
-		if (myOffer && !imInitiator) {
-			socket.emit('signal', myOffer)
-		}
-		isInARoom = true
-	})
-
-	document.getElementById('room3').addEventListener('click', () => {
-		if (myOffer && !imInitiator) {
-			socket.emit('signal', myOffer)
-		}
-		socket.emit('join', 'room3')
-		isInARoom = true
+		const newVideo = createNewVideoElement()
+		bindVideoToHtml(newVideo, stream)
+		// => Bind the stream that we recieve from the other peer into remoteVideo html element
 	})
 }
 
-const createPeer = stream =>
+const createPeer = initiator =>
 	new Peer({
-		initiator: true,
+		initiator: initiator,
 		trickle: false,
-		stream: stream,
 		config: {
 			iceServers: [
 				{
-					urls: [
-						'stun:stun.l.google.com:19302',
-						'stun:stun1.l.google.com:19302',
-						'stun:stun2.l.google.com:19302',
-						'stun:stun.l.google.com:19302?transport=udp'
-					]
+					urls: ['stun:stun.l.google.com:19302']
 				},
 				{
 					urls: 'turn:webrtcweb.com:7788',
@@ -102,14 +106,24 @@ const bindVideoToHtml = (DOMElement, stream) => {
 	if ('srcObject' in DOMElement) {
 		DOMElement.srcObject = stream
 	} else {
-		DOMElement.src = window.URL.createObjectURL(stream) // => For older browsers
+		DOMElement.src = window.URL.createObjectURL(stream)
+		// => For older browsers
 	}
+	DOMElement.play()
+}
+
+const createNewVideoElement = () => {
+	const body = document.getElementById('body')
+	const newVideoElement = document.createElement('video')
+	body.appendChild(newVideoElement)
+
+	return newVideoElement
 }
 
 const sale = async () => {
 	try {
 		const stream = await navigator.mediaDevices.getUserMedia({
-			video: false,
+			video: true,
 			audio: true
 		})
 		gotUserMedia(stream)
